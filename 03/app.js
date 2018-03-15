f9b501ad09e67dce72d4c6e8986a40e58260ca55
Unknown <1192395260@qq.com>
2018-03-15 15:26:52 +0800

    定义函数

--- 03/app.js
+++ 03/app.js
@@ -1,6 +1,15 @@
-var person = {
-	firstname: "Bucky",
-	lastname:"Roberts",
-	age:28
-	};
-console.log(person);
+// 01创建变量
+ // var person = {
+// 	firstname: "Bucky",
+// 	lastname:"Roberts",
+// 	age:28
+// 	};
+// console.log(person);
+
+
+//02创建函数
+function addNumber(a,b){
+	return a+b;
+}
+
+console.log(addNumber(11,11));
