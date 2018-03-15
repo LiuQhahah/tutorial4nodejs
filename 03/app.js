50ee36a8fe26602862f7a39a90c59397a9480068
Unknown <1192395260@qq.com>
2018-03-15 15:31:02 +0800

    定义变量函数以及延迟函数

--- 03/app.js
+++ 03/app.js
@@ -8,8 +8,15 @@
 
 
 //02创建函数
-function addNumber(a,b){
-	return a+b;
-}
+// function addNumber(a,b){
+// 	return a+b;
+// }
+//
+// console.log(addNumber(11,11));
+//03定义变量函数以及延迟函数
+var printBacon = function(){
+	console.log("Bacon is healthy ,dont believe the doctors");
+};//注有分号结束符
+printBacon();//此时有括号，而不仅仅是变量名
 
-console.log(addNumber(11,11));
+setTimeout(printBacon,3000);//此时没有括号，奇怪
